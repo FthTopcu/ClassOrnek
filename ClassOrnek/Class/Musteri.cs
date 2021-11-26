19cb4a4998f78d7bce1fe29f1118f34c6f323114
FthTopcu <fthtpu@gmail.com>
2021-11-26 21:24:29 +0300

    Add project files.

---
+++ ClassOrnek/Class/Musteri.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOrnek.Class
+{
+    class Musteri
+    {
+        #region Properties
+
+        public int MusteriId { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyAdi { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public Cinsiyetler Cinsiyeti { get; set; }
+        public UrunSepeti MusterininUrunSepeti { get; set; }
+
+        #endregion
+
+        #region Metotlar
+
+        public bool MusterininDogumGunuMu()
+        {
+            bool kontrol = false;
+
+            kontrol = (DogumTarihi.Day == DateTime.Now.Day && DogumTarihi.Month == DateTime.Now.Month) ? true : false;
+
+            return kontrol;
+        }
+
+        public void MusteriBilgileriYazdir()
+        {
+            Console.WriteLine($"Müşteri No : {MusteriId} - {MusteriAdi} {MusteriSoyAdi}");
+        }
+
+        public void MusterininSepetiniYazdir()
+        {
+            
+        }
+        #endregion
+
+    }
+}
---
+++ ClassOrnek/Class/UrunSepeti.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOrnek.Class
+{
+    class UrunSepeti
+    {
+        public int SepetId { get; set; }
+        public int MusteriId { get; set; }
+        public List<Urun> UrunlerListesi { get; set; } = new List<Urun>();
+
+        public void DogumGunuHediyesiEkle()
+        {
+            Urun hediye = new Urun()
+            {
+                UrunAdi = "Gofret",
+                UrunId = 300,
+                UrunKategorisi = UrunKategorileri.Gıda
+            };
+            UrunlerListesi.Add(hediye);
+        }
+    }
+}
